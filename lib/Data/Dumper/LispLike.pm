f82d786a9022993193f92a0d3dcac72e58aea981
Sergey Redin <sergey@redin.info>
2012-12-26 18:44:46 +0400

    docs

--- lib/Data/Dumper/LispLike.pm
+++ lib/Data/Dumper/LispLike.pm
@@ -1,9 +1,10 @@
 use strict;
 use warnings;
+
 package Data::Dumper::LispLike;
+# ABSTRACT: Dump perl data structures formatted as Lisp-like S-expressions
 
 use Exporter ();
-our $VERSION = 0.001;
 our @ISA = qw(Exporter);
 our @EXPORT = qw(&dumplisp);
 
@@ -63,6 +64,17 @@ sub dumplisp_iter($;$$) {
 	}
 }
 
+=func dumplisp
+
+    my $listref = ...;
+    print dumplisp $listref;
+
+This function converts an C<ARRAYREF>, which may contain strings or other
+C<ARRAYREF>s, into Lisp-like S-expressions. The output is much compacter
+and easier to read than the output of C<Data::Dumper>.
+
+=cut
+
 sub dumplisp($) {
 	1 == @_ or die "Usage: dumplisp(<expression>)\n";
 	my $out = dumplisp_iter shift;
@@ -72,3 +84,62 @@ sub dumplisp($) {
 }
 
 1;
+
+=head1 SYNOPSIS
+
+    use Data::Dumper::LispLike;
+    print dumplisp [ 1, 2, [3, 4] ]; # prints "(1 2 (3 4))\n";
+
+=for Pod::Coverage method_names_here
+
+=head1 EXAMPLE
+
+Here is a bigger real-life example of dumplisp() output:
+
+    (COMMA
+        (AND
+            (CMDDEF
+                echo
+                (%str)
+                (BLOCK (CMDRUN printf '%str\n')))
+            (CMDDEF
+                echo1
+                (%STR)
+                (BLOCK (CMDRUN print1 '%STR\n')))
+            (CMDDEF kill () (BLOCK (CMDRUN signal KILL)))
+            (CMDDEF term () (BLOCK (CMDRUN signal TERM)))
+            (CMDDEF hup () (BLOCK (CMDRUN signal HUP)))
+            (CMDDEF ps () (BLOCK (CMDRUN exec ps uf '{}')))
+            (CMDDEF
+                pso
+                (%PS_FIELDS)
+                (BLOCK (CMDRUN exec ps '-o\-' %PS_FIELDS '{}')))
+            (CMDDEF
+                exe
+                (%exe_arg)
+                (BLOCK (COMPARE == %exe %exe_arg)))
+            (CMDDEF
+                cwd
+                (%cwd_arg)
+                (BLOCK (COMPARE == %cwd %cwd_arg)))
+            (ASSIGN %vsz %statm::size)
+            (ASSIGN %rss %statm::resident)
+            (CMDDEF kthread () (BLOCK (COMPARE == 0 %rss)))
+            (CMDDEF
+                userspace
+                ()
+                (BLOCK (NOT (CMDRUN kthread))))
+            (ASSIGN %ppid %stat::ppid)
+            (ASSIGN %comm %stat::comm)
+            (ASSIGN %state %stat::state))
+        (AND
+            (BLOCK (OR (CMDRUN userspace) (COMPARE == %pid 2)))
+            (AND
+                (BLOCK
+                    (OR (CMDRUN userspace) (COMPARE == %pid 23)))
+            (CMDRUN ps)))
+
+=head1 SUPPORT
+
+L<http://github.com/spiculator/data-dumper-lisplike>
+
